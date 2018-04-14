8dbe933e9bc63836f993ac61fc95b23f3ea5b259
Codophile1 <yanisouakrim@yahoo.fr>
2018-04-14 17:58:46 +0200

    RideController

--- Controller/RideController.php
+++ Controller/RideController.php
@@ -10,13 +10,20 @@ class RideController
     /**
      *
      */
+     public $DAO;
 
     public function __construct()
     {
+        $DAO = new DAO();
     }
 
     public function displayAddRideForm(){
-
+        new AddRideForm();
     }
-    
+
+    public function displayEditRideForm($ride_id){
+      $DAO->
+      new EditRideForm();
+    }
+
 }
