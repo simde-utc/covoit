ddc74ddeecf0a7feed4ce8b1b2e02f064592610a
NastuzziSamy <samy@nastuzzi.fr>
2018-04-15 01:52:04 +0200

    Gestion de l'api GoogleMaps

---
+++ Services/GoogleMaps.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Services;
+
+class GoogleMaps {
+	protected $url;
+	protected $key;
+
+	public function __construct() {
+		$config = config('google');
+
+		$this->url = $config['url'];
+		$this->key = $config['key'];
+	}
+
+	protected function call($url, $queries = []) {
+		return json_decode(file_get_contents($this->url.$url.'?'.http_build_query(array_merge(
+			[ 'key' => $this->key ],
+			$queries
+		))), true);
+	}
+
+	public function ride($origins, $destinations) {
+		return $this->call('distancematrix/json', [
+			'origins' => $origins,
+			'destinations' => $destinations,
+		]);
+	}
+}
