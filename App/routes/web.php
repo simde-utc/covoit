e106239c36c857bd49e2fb0be9a3d19592edd9d4
Hive <hippolyte.vergnol@etu.utc.fr>
2018-04-15 00:31:18 +0200

    Car routes

--- App/routes/web.php
+++ App/routes/web.php
@@ -9,5 +9,12 @@ Route::get('/ride/{id}', 'RideController@displayRide', 'isLogged');
 Route::get('/addride', 'RideController@displayAddRideForm', 'isLogged');
 Route::post('/submitRide', 'RideController@processAddRide', 'isLogged');
 
+// Routes for CarController
+Route::get('/addcar', 'CarController@displayAddCarForm', 'isLogged');
+Route::post('/submitCar', 'CarController@processAddCar', 'isLogged');
+Route::get('/car/view/{id}', 'CarController@displayCar');
+Route::get('/editCar/{id}', 'CarController@displayEditCarForm', 'isLogged');
+Route::post('/editCar/editCar', 'CarController@processEditCarForm', 'isLogged');
+
 echo 'PAS DE PAGE TROUVEE !!';
 exit;
