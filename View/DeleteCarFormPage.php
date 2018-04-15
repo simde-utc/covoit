93698862216e21e0d1a26f177d7c3bc0b237b516
williamboffy <williamboffy@hotmail.fr>
2018-04-15 04:11:14 +0200

    [view] debug

--- View/DeleteCarFormPage.php
+++ View/DeleteCarFormPage.php
@@ -1,19 +1,35 @@
 <?php
 
-class DeleteCarFormPage
-{
-    function __construct($CarsObject)
-    {
+require_once("Page.php");
 
-        ?>
-        <form method="POST">
-            <select name="idCar">
-                <?php foreach ($CarsObject as $car){
-                    echo '<option value="'.$car['idCar'].'">'.$car['model'].' '.$car['color'].'</option>';
-                }?>
-            </select>
-            <input type="submit" />
-        </form>
-        <?php
+class DeleteCarFormPage extends Page
+{
+    private $cars = null;
+    public function __construct($CarsObject, $title="", $author="", $desc=""){
+        parent::__construct($title, $author, $desc);
+        $this->cars = $CarsObject;
     }
+ 
+    public function generateContent(){
+        $this->content = <<<HTML
+        <form action="submitCar" method="POST">
+          <div class="form-group">
+            <label for="color">Voiture</label>
+            <select class="form-control" name="idCar">
+HTML
+;
+        var_dump($this->cars);
+        foreach($this->cars as $c){
+            $this->content .= "<option value='{$c['idCar']}'>{$c['model']} {$c['color']}</option>";                
+        }
+        
+        $this->content .= <<<HTML
+            </select>
+          </div>
+          <button type="submit" class="btn btn-primary">Envoyer</button>
+        </form>
+HTML
+;
+    }   
+    
 }
