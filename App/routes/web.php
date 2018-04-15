7606921a0f6d1f28592172f4bdc79f97fcc9c7b7
NastuzziSamy <samy@nastuzzi.fr>
2018-04-15 03:55:45 +0200

    Réaménagement des routes

--- App/routes/web.php
+++ App/routes/web.php
@@ -5,24 +5,20 @@ namespace App;
 Route::get('/login', 'LoginController@index', 'isNotLogged');
 Route::get('/login/process', 'LoginController@login', 'isNotLogged');
 Route::get('/logout', 'LoginController@logout', 'isLogged');
-Route::get('/ride/{id}', 'RideController@displayRide', 'isLogged');
-Route::get('/addride', 'RideController@displayAddRideForm', 'isLogged');
-Route::post('/submitRide', 'RideController@processAddRide', 'isLogged');
 
 // Routes for CarController
-Route::get('/addcar', 'CarController@displayAddCarForm', 'isLogged');
-Route::post('/submitCar', 'CarController@processAddCar', 'isLogged');
-Route::get('/car/view/{id}', 'CarController@displayCar', 'isLogged');
-Route::get('/editCar/{id}', 'CarController@displayEditCarForm', 'isLogged');
-Route::post('/editCar/editCar', 'CarController@processEditCarForm', 'isLogged');
-Route::post('/deleteCar', 'CarController@processDeleteCar', 'isLogged');
-Route::get('/deleteCar', 'CarController@displayDeleteCarForm', 'isLogged');
-
-
-
+Route::get('/cars', 'CarController@displayCars', 'isLogged');
+Route::get('/cars/add', 'CarController@displayAddCarForm', 'isLogged');
+Route::post('/cars/add', 'CarController@processAddCar', 'isLogged');
+Route::get('/cars/{id}', 'CarController@displayCar', 'isLogged');
+Route::get('/cars/{id}/edit', 'CarController@displayEditCarForm', 'isLogged');
+Route::post('/cars/{id}/edit', 'CarController@processEditCarForm', 'isLogged');
+Route::get('/cars/{id}/delete', 'CarController@processDeleteCar', 'isLogged');
 
 // Routes for RideController
-Route::get('/ride/view/{id}', 'RideController@displayRide', 'isLogged');
+Route::get('/rides/add', 'RideController@displayAddRideForm', 'isLogged');
+Route::post('/rides/add', 'RideController@processAddRide', 'isLogged');
+Route::get('/rides/{id}', 'RideController@displayRide', 'isLogged');
 
 echo 'PAS DE PAGE TROUVEE !!';
 exit;
